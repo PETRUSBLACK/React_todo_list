2786c8d2a06f131e545852437dd7ef0b96f2d871
Petrus Onukwu <onukwupetrusoge@gmail.com>
2025-04-24 15:06:55 +0100

    props

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,12 @@
 import { useState } from "react"
 import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
 
 export default function App(){
 
-  const [newItem, setNewItem] = useState(" ")
   const [todos, setTodos] = useState([])
 
-  function handleSubmit(e){
-    e.preventDefault()
 
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-      { id: crypto.randomUUID(), title: newItem, completed: false},
-    ]
-    })
-
-    setNewItem("")
-
-  }
   
   function toggleTodo( id, completed){
 
@@ -42,23 +30,14 @@ export default function App(){
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-          <div className="form-row">
-              <label htmlFor="item"> New Item</label>
-              <input 
-                value = {newItem}
-                onChange = { e => setNewItem(e.target.value)}
-                type="text" 
-                id="item"/>
-          </div>
-          <button className="btn">Add</button>      
-      </form>
+
 
       <h1 className="header">Todo List</h1>
+      <NewTodoForm />
 
       <ul className="list">
         {todos.length === 0 && "No Todos"}
-        {todos.map( todo => {
+        {todos.map( todo => {setTodos
           return <li key={todo.id}>
                     <label>
                       <input type="checkbox" 
